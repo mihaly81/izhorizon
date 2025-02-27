ae5b3884d18e9dafd30535cfbba876ebe348bce0
mihaly81 <gacsomihaly1@gmail.com>
2025-02-27 14:02:40 +0100

    bcryptjs hozzáadva package.jsonbe

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
